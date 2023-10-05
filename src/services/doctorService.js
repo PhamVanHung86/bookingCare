1e8ed651a05ef6196c097908e922bbd0b40b1f17
PhamVanHung86 <hung09058@gmail.com>
2023-10-06 02:29:28 +0700

    #67

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -5,21 +5,21 @@ let getTopDoctorHome = (limit) => {
     try {
       let users = await db.User.findAll({
         limit: limit,
-        where: { roleId: 'R2' },
-        order: [['createdAt', 'DESC']],
+        where: { roleId: "R2" },
+        order: [["createdAt", "DESC"]],
         attributes: {
-          exclude: ['password'],
+          exclude: ["password"],
         },
         include: [
           {
             model: db.Allcode,
-            as: 'positionData',
-            attributes: ['valueEn', 'valueVi'],
+            as: "positionData",
+            attributes: ["valueEn", "valueVi"],
           },
           {
             model: db.Allcode,
-            as: 'genderData',
-            attributes: ['valueEn', 'valueVi'],
+            as: "genderData",
+            attributes: ["valueEn", "valueVi"],
           },
         ],
         raw: true,
@@ -36,6 +36,58 @@ let getTopDoctorHome = (limit) => {
   });
 };
 
+let getAllDoctors = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let doctors = await db.User.findAll({
+        where: { roleId: "R2" },
+        attributes: {
+          exclude: ["password", "image"],
+        },
+      });
+      resolve({
+        errCode: 0,
+        data: doctors,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let saveInfoDoctor = (inputData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (
+        !inputData.doctorId ||
+        !inputData.contentHTML ||
+        !inputData.contentMarkdown
+      ) {
+        resolve({
+          errCode: 1,
+          errMessage: "missing parameter",
+        });
+      } else {
+        await db.Markdown.create({
+          contentMarkdown: inputData.contentMarkdown,
+          contentHTML: inputData.contentHTML,
+          description: inputData.description,
+          doctorId: inputData.doctorId,
+        });
+
+        resolve({
+          errCode: 0,
+          errMessage: "Save info success",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
+  getAllDoctors: getAllDoctors,
+  saveInfoDoctor: saveInfoDoctor,
 };
